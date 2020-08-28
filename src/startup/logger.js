fc8f083edb78244dd9876fb52806f3a84a46e281
Andon <andon.design@gmail.com>
2020-08-28 09:26:12 -0400

    test mail

--- src/startup/logger.js
+++ src/startup/logger.js
@@ -3,6 +3,11 @@ const Sentry = require('@sentry/node');
 
 exports.init = function () {
   Sentry.init({ dsn: process.env.SentryDSN_URL });
+  Sentry.init({
+    ignoreErrors: [
+      'Non-Error exception captured',
+    ],
+  });
 };
 
 exports.logInfo = function (message) {
